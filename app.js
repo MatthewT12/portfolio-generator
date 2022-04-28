3481c9c86a932825d6a61e12e325a288dadb3548
MatthewT12 <matttyndell@gmail.com>
2022-04-28 19:06:48 -0400

    learning template literals and interpolation

--- app.js
+++ app.js
@@ -1,10 +1,34 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+const generatePage = (name, github) => {
+    
+    return `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta hhtp-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Portfolio Demo</title>
+        </head>
 
+        <body>
+            <h1>${name}</h1>
+            <h2><1 href="https://github.com/${github}">GitHub</a><h2>
+        </body>
+        </html>
+    
+    `;
+        
+};   
+console.log(name, github);
+console.log(generatePage(name, github));
+//console.log(generatePage());
 
-console.log(profileDataArgs);
+/*console.log(profileDataArgs);
 
 const printProfileData = profileDataArr => {
     profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
-printProfileData(profileDataArgs);
+printProfileData(profileDataArgs);*/
