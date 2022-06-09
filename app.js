8bc63247b38ff03f57d1e714bd60d1d59276bac0
MatthewT12 <matttyndell@gmail.com>
2022-06-09 17:31:25 -0400

    added module.export/cleaned up code

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 const [name, github] = profileDataArgs;
-const generatePage = (name, github) => {
-    
-    return `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <meta hhtp-equiv="X-UA-Compatible" content="ie=edge">
-            <title>Portfolio Demo</title>
-        </head>
 
-        <body>
-            <h1>${name}</h1>
-            <h2><1 href="https://github.com/${github}">GitHub</a><h2>
-        </body>
-        </html>
-    
-    `;
-        
-};   
-console.log(name, github);
-console.log(generatePage(name, github));
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
 //console.log(generatePage());
 
 /*console.log(profileDataArgs);
